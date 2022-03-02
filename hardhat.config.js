d5a06c6cae0dc99a540a54ddc303fa20d472b165
K1-R1 <kyranrawlinson@gmail.com>
2022-03-02 22:04:15 +0000

    Updated hardhat.config.js

--- hardhat.config.js
+++ hardhat.config.js
@@ -18,5 +18,10 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
  * @type import('hardhat/config').HardhatUserConfig
  */
 module.exports = {
-  solidity: "0.8.4",
+  solidity: {
+    compilers: [
+      { version: "0.8.4" },
+      { version: "0.6.0" }
+    ]
+  }
 };
